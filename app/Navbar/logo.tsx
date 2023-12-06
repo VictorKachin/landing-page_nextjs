e22efe3f22a2a56e4e047f25f68bbbb8d405069c
VictorKachin <victor.kachin@yandex.ru>
2023-12-06 23:17:02 +0300

    Update logo.tsx

--- app/Navbar/logo.tsx
+++ app/Navbar/logo.tsx
@@ -1,19 +1,14 @@
 import Image from 'next/image'
 
-
-// 16:50 
+// 16:50 from
 // https://www.youtube.com/watch?v=Z0cn02PTKBQ&ab_channel=iggy
 
 const Logo = () => {
-	return <div>
-		
-		<Image
-			src='/images/logo/logo-1.svg'
-			width={50}
-			height={50}
-			alt='logo'
-		/>
-	</div>
+	return (
+		<div>
+			<Image src='/images/logo/logo-1.svg' width={50} height={50} alt='logo' />
+		</div>
+	)
 }
 
 export default Logo
