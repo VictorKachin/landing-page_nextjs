f7474e15be63d81f6ce3159cae66723d14ca22db
VictorKachin <victor.kachin@yandex.ru>
2023-12-13 19:02:58 +0300

    change page.tsx

--- app/Navbar/page.tsx
+++ app/Navbar/page.tsx
@@ -3,19 +3,13 @@ import Logo from './logo'
 import NavigationBar from './navigation-bar'
 
 const Navbar = () => {
-	return (
-		<div
-			className='
-		flex
-		justify-between
-		items-center px-10 border-b
-		'
-		>
-			<Logo />
-			<NavigationBar />
-			<ActionButtons />
-		</div>
-	)
-}
+  return (
+    <div className="flex justify-between items-center px-10 border-b">
+      <Logo />
+      <NavigationBar />
+      <ActionButtons />
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
